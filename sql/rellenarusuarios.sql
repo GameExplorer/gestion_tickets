ed2c64190c5bfa305535cec48b66072dd273bb26
ajdortagonzalez <51479152+ajdortagonzalez@users.noreply.github.com>
2024-05-02 23:09:09 +0100

    Add files via upload

---
+++ sql/rellenarusuarios.sql
@@ -0,0 +1,6 @@
+INSERT INTO `usuarios`(`nombre_usuario`, `pass_usuario`, `id_departamento`)
+ VALUES ('juanfran','informatica2024','1');
+ INSERT INTO `usuarios`(`nombre_usuario`, `pass_usuario`, `id_departamento`)
+ VALUES ('test','rrhh2024','2');
+ INSERT INTO `usuarios`(`nombre_usuario`, `pass_usuario`, `id_departamento`)
+ VALUES ('usuariofinanzas','finanzas2024','3');

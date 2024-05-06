2eb7d8883e560edcab4f35584ec5ea840d54b9cc
GameExplorer <petar.petrovic2001@gmail.com>
2024-05-06 10:59:08 +0100

    Menu Updated

--- includes/menu.php
+++ includes/menu.php
@@ -5,106 +5,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Responsive Navbar</title>
+        <title>Navbar</title>
+        <link rel="stylesheet" href="css/menu_style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-        <style>
-            .navbar-custom {
-                position: sticky;
-                background-color: #f8f9fa;
-                width: 100%;
-                margin-bottom: 2.5rem;
-            }
-
-            .navbar-brand img {
-                width: 72px;
-                height: auto;
-                margin-right: 10px;
-            }
-
-            .navbar-nav .nav-link {
-                color: #343a40;
-                font-size: 1.25em;
-                margin-right: 12px;
-                margin-left: 8px;
-                text-transform: uppercase;
-            }
-
-            .title {
-                font-size: 2em;
-                font-weight: 500;
-                text-align: center;
-                text-transform: uppercase;
-                margin-left: 10rem;
-            }
-
-            .LocationText {
-                text-align: center;
-                font-size: 1em;
-                color: #6c757d;
-            }
-
-            @media (max-width: 991.98px) {
-                .navbar-collapse {
-                    display: block;
-                    text-align: center;
-                }
-
-                .navbar-nav {
-                    display: inline-block;
-                    margin-top: 10px;
-                }
-
-                .title {
-                    display: inline-block;
-                    margin-left: 10px;
-                }
-            }
-
-            /* Nav items hover effect */
-            .underline {
-                display: inline;
-                position: relative;
-                overflow: hidden;
-            }
-
-            .underline:after {
-                content: "";
-                position: absolute;
-                z-index: -1;
-                right: 0;
-                width: 0;
-                bottom: -5px;
-                background: #000;
-                height: 2px;
-                transition: width 0.4s ease-out;
-            }
-
-            .underline:hover:after,
-            .underline:focus:after,
-            .underline:active:after {
-                left: 0;
-                right: auto;
-                width: 100%;
-            }
-
-            /* Login button */
-            .login {
-                width: 140px;
-                height: auto;
-                background-color: #96c565;
-                text-align: center;
-                border-radius: 10px;
-            }
-
-            .login:hover {
-                background-color: inherit;
-                color: #96c565;
-                border: 3px solid #96c565;
-                transition: background-color 0.4s ease-out, color 0.4s ease-out, border-color 0.4s ease-out;
-            }
-        </style>
     </head>
 
     <body>

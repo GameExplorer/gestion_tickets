8a1d578933af959a599a91ea2bd7ceff8882d2c5
GameExplorer <petar.petrovic2001@gmail.com>
2024-05-06 10:46:39 +0100

    Ticket form style update

--- ticket_form.php
+++ ticket_form.php
@@ -22,13 +22,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>DETALLES TICKET</title>
         <link rel="stylesheet" href="css/ticket_form_style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="js/ticket_form_script.js"></script>
     </head>
@@ -118,22 +121,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="row mb-3">
                     <label for="attachment" class="col-md-3 col-form-label">Adjuntos:</label>
                     <div class="col-md-9">
-                        <div class="row"> 
-                            <div class="col-8"> 
+                        <div class="row">
+                            <div class="col-12 col-sm-8 order-sm-1 order-1">
                                 <div id="attachment-container">
                                     <input type="file" id="attachment" name="attachment[]"
                                         accept=".pdf, .png, .jpg, .jpeg" class="form-control">
                                 </div>
                             </div>
-                            <div class="col-3"> 
-                                <button type="button" onclick="addAttachment()" class="btn btn-primary"
-                                    style="background-color: #4caf50; color: white; border: none; cursor: pointer; width: 100%; height: 70%; margin-top: 12.5px;margin-left: 10px;">
-                                    Añadir otro</button>
+                            <div class="col-12 col-sm-3 order-sm-2 order-2">
+                                <button type="button" onclick="addAttachment()" class="btn btn-sucess"
+                                    style="background-color: #4caf50; color: white; border: none; cursor: pointer; width: auto; height:70%; margin-top: 12.5px; margin-left: 10px;">
+                                    Añadir otro
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
-
 
                 <script>
                     function addAttachment() {

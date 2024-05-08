156a8d71896e2bafc7482830a79c4253af5622fb
GameExplorer <petar.petrovic2001@gmail.com>
2024-05-08 20:04:05 +0100

    Minor fixes to login.php

--- login.php
+++ login.php
@@ -1,10 +1,23 @@
+<?php
+
+session_start();
+
+include 'includes/connection.php';
+
+?>
 <!DOCTYPE html>
 <html>
 
     <head>
         <meta charset="UTF-8">
-        <title>Página Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link rel="stylesheet" href="css/login_style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/ticket_table_script.js"></script>
+        <title>Acceso</title>
     </head>
 
     <body>

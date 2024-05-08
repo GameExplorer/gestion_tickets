0a66f28c4cf3a6773f142146a915c8e69b8ebde8
ajdortagonzalez <51479152+ajdortagonzalez@users.noreply.github.com>
2024-05-09 02:24:57 +0900

    Add files via upload

--- login.php
+++ login.php
@@ -1,49 +1,38 @@
-<?php
-session_start();
-
-include 'connection.php';
-?>
 <!DOCTYPE html>
 <html>
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <title>Página Login</title>
         <link rel="stylesheet" href="css/login_style.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="js/ticket_table_script.js"></script>
-        <title>Acceso</title>
     </head>
 
     <body>
         <div class="container">
-
-                           <form method="post">
-                <h1>ACCESO</h1>
-                <label for="username">Username:</label>
+            <form method="post">
+                <h1>LOGIN</h1>
+                <label for="username">Usuario:</label>
                 <input id="username" name="username" minlength="4" maxLength="32" type="text" class="inputStyle"
-                    placeholder="Enter a username" required />
+                    placeholder="Introduzca nombre de usuario" required />
 
-                <label for="password">Password:</label>
+                <label for="password">Contraseña:</label>
                 <input id="password" name="password" minlength="6" maxlength="16" required type="password"
-                    class="inputStyle" placeholder="Enter a password" />
+                    class="inputStyle" placeholder="Introduzca contraseña" />
                 <input type="checkbox" class="show-password"
-                    onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"> Show
-                password<br>
+                    onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"> Mostrar contraseña<br>
                 <?php
+                session_start();
 
+                include 'includes/connection.php';
 
                 if (isset($_POST['login'])) {
                     // Get the form data
-                    $username = $_POST['username'];
+                    $nombreUsuario = $_POST['username'];
                     $password = $_POST['password'];
 
                     // Prepare SQL statement
-                    $stmt = $conn->prepare("SELECT User_ID, Pass, Department_ID FROM Users WHERE Username = ?");// this
-                    $stmt->bind_param("s", $username);
+                    $stmt = $conn->prepare("SELECT id_usuario, pass_usuario, id_departamento FROM usuarios WHERE nombre_usuario = ?");
+                    $stmt->bind_param("s", $nombreUsuario);
 
                     // Execute the statement
                     $stmt->execute();
@@ -51,7 +40,7 @@ include 'connection.php';
 
                     // Checks if user exists
                     if ($stmt->num_rows > 0) {
-                        $stmt->bind_result($id, $hashed_password, $loggedDepartmentId); // BIND Department_ID
+                        $stmt->bind_result($id, $hashed_password, $idDepartamentoLogeado);
                 
                         // Fetch the result
                         $stmt->fetch();
@@ -60,16 +49,20 @@ include 'connection.php';
                         if ($password === $hashed_password) {
                             $_SESSION['loggedin'] = true;
                             $_SESSION['id'] = $id;
-                            $_SESSION['username'] = $username;
-                            $_SESSION['department_id'] = $loggedDepartmentId; // STORE Department_ID 
+                            $_SESSION['username'] = $nombreUsuario;
+                            $_SESSION['department_id'] = $idDepartamentoLogeado;
                 
+                            if (empty($_SERVER['REMOTE_ADDR'])) {
+                                $_SESSION['location'] = ''; // Set default location here
+                            }
+
                             header("Location: ticket_table.php");
                             exit;
                         } else {
-                            echo "<div class='errorMessage'>There was an error signing in. Incorrect Password</div>";
+                            echo "<div class='errorMessage'>Ha habido un error. Contraseña incorrecta.</div>";
                         }
                     } else {
-                        echo "<div class='errorMessage'>User doesn't exist. Not found</div>";
+                        echo "<div class='errorMessage'>El usuario introducido no existe</div>";
                     }
 
                     $stmt->close();
@@ -78,7 +71,7 @@ include 'connection.php';
                 ?>
 
 
-                <input name="login" type="submit" value="Acceso" class="login" />
+                <input name="login" type="submit" value="Acceder" class="login" />
             </form>
         </div>
 
@@ -86,4 +79,3 @@ include 'connection.php';
     </body>
 
 </html>
-

70308ae0055c49aee0d4d10e385c90903fa944bb
ajdortagonzalez <51479152+ajdortagonzalez@users.noreply.github.com>
2024-05-15 23:36:50 +0100

    dynamic attachment path

--- includes/upload_config.php
+++ includes/upload_config.php
@@ -1,6 +1,8 @@
 <?php
 // Define upload directory, file type and size restrictions
-$targetDir = __DIR__ . "/../adjuntos/";
+//WARNING: If you change the targetDir, it won't move files to the new folder.
+//Also make sure the folder exists and have all permissions. Default one is $targetDir = "adjuntos/";
+$targetDir = "adjuntos/";
 $fileTypeRestrictions = array("pdf", "png", "jpg", "jpeg");
 $maxFileSizeMB = 20; // in megabytes
 
